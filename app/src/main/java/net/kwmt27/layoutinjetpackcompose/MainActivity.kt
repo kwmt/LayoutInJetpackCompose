b446c2e6b2be4773c9436a5eb2f32650872aa9b3
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-02-02 01:24:18 +0900

    row

--- app/src/main/java/net/kwmt27/layoutinjetpackcompose/MainActivity.kt
+++ app/src/main/java/net/kwmt27/layoutinjetpackcompose/MainActivity.kt
@@ -3,12 +3,17 @@ package net.kwmt27.layoutinjetpackcompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Providers
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.kwmt27.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
 
 class MainActivity : AppCompatActivity() {
@@ -40,10 +45,19 @@ fun DefaultPreview() {
 
 @Composable
 fun PhotographerCard() {
-    Column {
-        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-        Providers( AmbientContentAlpha provides ContentAlpha.medium) {
-            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+    Row {
+        Surface(
+            modifier = Modifier.preferredSize(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // image goes here
+        }
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            Providers(AmbientContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
@@ -51,7 +65,7 @@ fun PhotographerCard() {
 @Preview
 @Composable
 fun PhotographerCardPreview() {
-    LayoutInJetpackComposeTheme{
+    LayoutInJetpackComposeTheme {
         PhotographerCard()
     }
 }
