6f74c1830f35a4cf20d49048ea23ccc51f6b8c52
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-02-02 01:01:33 +0900

    レイアウトする前のレイアウトを記入

--- app/src/main/java/net/kwmt27/layoutinjetpackcompose/MainActivity.kt
+++ app/src/main/java/net/kwmt27/layoutinjetpackcompose/MainActivity.kt
@@ -2,11 +2,12 @@ package net.kwmt27.layoutinjetpackcompose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import net.kwmt27.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
 
@@ -34,5 +35,23 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     LayoutInJetpackComposeTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun PhotographerCard() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        Providers( AmbientContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutInJetpackComposeTheme{
+        PhotographerCard()
     }
 }
