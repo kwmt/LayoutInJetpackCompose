986596d435bd5f089b60127fa84a59204413aa5e
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-02-01 23:32:52 +0900

    first commit

---
+++ app/src/main/java/net/kwmt27/layoutinjetpackcompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package net.kwmt27.layoutinjetpackcompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import net.kwmt27.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutInJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutInJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
