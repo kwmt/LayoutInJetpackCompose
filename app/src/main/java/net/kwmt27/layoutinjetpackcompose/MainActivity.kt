0c3d89b53a641cd89e1f78daa5fa085f51abc6a8
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-02-06 22:26:38 +0900

    カスタムレイアウト

--- app/src/main/java/net/kwmt27/layoutinjetpackcompose/MainActivity.kt
+++ app/src/main/java/net/kwmt27/layoutinjetpackcompose/MainActivity.kt
@@ -20,10 +20,14 @@ import androidx.compose.runtime.Providers
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import net.kwmt27.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
 
@@ -150,10 +154,44 @@ fun BodyContent(modifier: Modifier = Modifier) {
     }
 }
 
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = Modifier.layout {measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    // Height of the composable with padding - first baseline
+    val placeableY = firstBaselineToTop.toIntPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        //...
+        placeable.placeRelative(0, placeableY)
+    }
+}
+
 @Preview
 @Composable
 fun PhotographerCardPreview() {
     LayoutInJetpackComposeTheme {
         LayoutsCodelab()
+    }
+}
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutInJetpackComposeTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutInJetpackComposeTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
